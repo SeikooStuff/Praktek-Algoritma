5807aefd1eaa94c174d1aa1de26ece23a9476508
SeikooStuff <113978620+SeikooStuff@users.noreply.github.com>
2022-09-22 18:35:38 +0700

    Add files via upload

---
+++ latihan2.py
@@ -0,0 +1,9 @@
+print('Hitung Luas Ruangan')
+
+p = float(input("Masukkan Panjang Ruangan: "))
+l = float(input("Masukkan Lebar Ruangan: "))
+s = input("Masukkan Satuan (Meter/Inci): ")
+
+luas = p*l
+
+print("Luas ruangan dengan panjang", p, "dan lebar", l, "adalah", luas, s)
